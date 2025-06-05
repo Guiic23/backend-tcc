367404bfb6f38b397733dd6112a20230d22437b5
Guilherme Rodrigues Santos <160981090+Guiic23@users.noreply.github.com>
2025-06-05 11:24:39 +0000

    [feat] Get Users and Payments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import express from 'express'
+import express from 'express';
+import rootRoutes from './routes/index.js';
 
-const server = express()
+
+const server = express();
 
 const PORT = process.env.PORT || 3000;
 
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use("/api", rootRoutes);
+
 server.get("/", (req, res) => {
   res.status(200).send("Primeira Rota do Backend");
-}
-);
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 const router = express.Router();
 
-import userRoutes from './paymentRoutes'
-import paymentRoutes from './paymentRoutes'
+import userRoutes from './userRoutes.js'
+import paymentRoutes from './paymentRoutes.js'
 
-router.use('/users', userRoutes);
+router.use('/', userRoutes);
+router.use('/', paymentRoutes);
+
+export default router;
+
 
