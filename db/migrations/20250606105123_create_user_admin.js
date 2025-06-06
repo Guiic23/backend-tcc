c1b4f10fc1183d23fe50710c0e4bc21a79d1c342
Guilherme Rodrigues Santos <160981090+Guiic23@users.noreply.github.com>
2025-06-06 11:05:43 +0000

    Commit

---
+++ db/migrations/20250606105123_create_user_admin.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+    return await knex('users').insert({
+      username: 'admin',
+      email: 'admin@admin.com',
+      password: 'admin',
+      role: 'admin'
+    });
+  }
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  export async function down(knex) {
+    return await knex('users').where({ username: 'admin' }).del();
+  }
+  
